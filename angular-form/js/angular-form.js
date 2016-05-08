9bc80ed629504b07de93e7bf5f752ba3de01181b
jeremy-denis <jeremy.denis9@hotmail.fr>
2016-05-08 20:43:09 +0200

    if the value of an attribute object is an array it generate a select

--- angular-form/js/angular-form.js
+++ angular-form/js/angular-form.js
@@ -22,7 +22,17 @@ function angularFormController($scope,$sce) {
 		vm.form = '<table>'
 		angular.forEach($scope.object, function(value, key) {
 		  vm.form += '<tr><td>'+key+' : </td>';
-		  vm.form += '<td><input type="text" value="'+value+'"/></td></tr>';
+		  if (typeof value == 'string' || !value instanceof Array) {
+				vm.form += '<td><input name="'+key+'" type="text" value="'+value+'"/></td></tr>';
+		  } else {
+			if (value instanceof Array) {
+				vm.form += '<td><select name="'+key+'">';
+				angular.forEach(value, function(option) {
+					vm.form += '<option value="'+option+'">'+option+'</option>';
+				});
+				vm.form += '</select></td></tr>';
+			}
+		  }
 		});
 		vm.form += '</table>';
 		vm.form = $sce.trustAsHtml(vm.form);
@@ -43,3 +53,4 @@ function angularForm() {
 		template:'<div ng-bind-html="ctrl.form"></div>',
 	}
 }
+
