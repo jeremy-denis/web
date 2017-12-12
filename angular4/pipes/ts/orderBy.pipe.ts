6121d33e0d1acff346eed9223e96ddfeaefa6b09
jeremy-denis <jeremy.denis9@hotmail.fr>
2017-12-12 19:14:46 +0100

    add constraint when array is undefined

--- angular4/pipes/ts/orderBy.pipe.ts
+++ angular4/pipes/ts/orderBy.pipe.ts
@@ -8,13 +8,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class OrderByPipe implements PipeTransform {
   transform(array: any, field: string): any {
-    array.sort((a: any, b: any) => {
-      if (a[field] < b[field]) {
-        return -1;
-      }
-      
-	  return (a[field] > b[field] ? 1 : 0);
-    });
+	if (array != undefined) {
+		array.sort((a: any, b: any) => {
+		  if (a[field] < b[field]) {
+			return -1;
+		  }
+		  
+		  return (a[field] > b[field] ? 1 : 0);
+		});
+    }
     return array;
   }
 }
